0a06a4bd8f2e90d88008a1d3a44a2cbee5408e4c
Daniel Jonasson <daniel@guadeo.com>
2014-05-22 16:58:39 +0200

    Refactored code and added some functionality.

--- lib/asset_manager/extensions/all.rb
+++
@@ -1,1 +0,0 @@
-require 'asset_manager/extensions/active_record'
--- spec/dummy/app/models/post.rb
+++ spec/dummy/app/models/post.rb
@@ -4,10 +4,11 @@ class Post < ActiveRecord::Base
   has_many :comments
 
   validate :title, presence: true
-  validate :sub_title, presence: true
 
   has_image :main_image
-  has_images :pictures
-  has_file :main_card
-  has_files :downloads
+  has_images :images
+  has_file :main_file
+  has_files :files
+  has_file :not_singulars
+  has_files :not_plural
 end
